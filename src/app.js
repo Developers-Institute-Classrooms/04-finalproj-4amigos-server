7528a5ecfbd4fadf835883dc11d1c3f9e1a100d2
Chris Anderson <chrisanderson@developersinstitute.co.nz>
2021-05-18 14:07:25 +1200

    created mock json data from jsonplaceholder.com

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const app = express();
 
 // middleware
-
 app.use(express.json());
 app.use(cors());
 
 // Routes
+const reviews = require("./data.json");
 
 // testing 'hello world' response on the '/' path
 app.get("/api", async (req, res) => {
