a1ac7eb23f8966fb6b1cf435bd8a8053c5fd2556
Chris Anderson <chrisanderson@developersinstitute.co.nz>
2021-05-19 11:21:50 +1200

    added nodemon to start script. added get one and get all routes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
+// Routes mock data
 const reviews = require("./data.json");
 
 // testing 'hello world' response on the '/' path
@@ -15,12 +15,19 @@ app.get("/api", async (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+// test healthcheck route
 app.get("/api/healthcheck", async (req, res) => {
   res.json({ message: "Health check working!" });
 });
 
+// get one
 app.get("/api/reviews", async (req, res) => {
-  res.json({ message: "Reviews Page working!" });
+  return res.json(reviews[0]);
+});
+
+// get all
+app.get("/api/all-reviews", async (req, res) => {
+  return res.json({ reviews });
 });
 
 module.exports = app;
