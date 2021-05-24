a240d55274c6895378f60104a1cd05fcd2c40b45
CarleeLaws <carleelaws@developersinstitute.co.nz>
2021-05-25 11:40:42 +1200

    sql values changed

--- src/services/reviews.js
+++ src/services/reviews.js
@@ -3,8 +3,8 @@ const pool = require("./db");
 // Create a review
 const createReview = async () => {
   const newReview = await pool.query(
-    "INSERT INTO review_persons VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-    [id, reviewer_name, email, q1, q2, q3, q4, q5, person_type]
+    "INSERT INTO review_persons VALUES (id, reviewer_name, email, q1, q2, q3, q4, q5, person_type) RETURNING *"
+    // [id, reviewer_name, email, q1, q2, q3, q4, q5, person_type]
   );
   return newReview;
 };
