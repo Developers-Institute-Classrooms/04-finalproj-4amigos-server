e28c9c696d59cf96e8a8d4e70b1fdd0405542c4f
Chris Anderson <chrisanderson@developersinstitute.co.nz>
2021-05-25 16:00:13 +1200

    changed message back, basically undone previous commit in file 2-3

--- db-migrations/2-3.sql
+++ db-migrations/2-3.sql
@@ -1,3 +1,3 @@
 BEGIN;
-INSERT INTO review_persons (id, person_name, term, class_name, person_type) VALUES ('Joe Bloggs', 'two', 'agile', 'student');
+INSERT INTO review_persons (person_name, term, class_name, person_type) VALUES ('Joe Bloggs', 'two', 'agile', 'student');
 COMMIT;
