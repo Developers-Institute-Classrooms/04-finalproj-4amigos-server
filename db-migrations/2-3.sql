202d001faca5a27259eed2c0f9b8d134c37f5e50
CarleeLaws <carleelaws@developersinstitute.co.nz>
2021-05-26 13:18:12 +1200

    mock data for the drop downs on the front end inserted

--- db-migrations/2-3.sql
+++ db-migrations/2-3.sql
@@ -1,3 +1,15 @@
 BEGIN;
-INSERT INTO review_persons (person_name, term, class_name, person_type) VALUES ('Joe Bloggs', 'two', 'agile', 'student');
+INSERT INTO review_persons (person_name, term, class_name, person_type)
+VALUES (
+        'Lance Cooper',
+        'T3-2020',
+        'client-side',
+        'instructor'
+    );
+INSERT INTO review_persons (person_name, term, class_name, person_type)
+VALUES ('Nik Ho', 'T4-2020', 'server-side', 'instructor');
+INSERT INTO review_persons (person_name, term, class_name, person_type)
+VALUES ('Nik Ho', 'T1-2021', 'Full-stack', 'instructor');
+INSERT INTO review_persons (person_name, term, class_name, person_type)
+VALUES ('Raf Gemmail', 'T2-2021', 'agile', 'instructor');
 COMMIT;
